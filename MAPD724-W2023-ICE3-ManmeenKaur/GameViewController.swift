013230851b20fa1433ecd445812b9b927d630d28
manmeenkaur <manmeen.kaur.17@gmail.com>
2023-02-05 23:25:04 -0500

    added lives and score

--- MAPD724-W2023-ICE3-ManmeenKaur/GameViewController.swift
+++ MAPD724-W2023-ICE3-ManmeenKaur/GameViewController.swift
@@ -4,6 +4,9 @@ import GameplayKit
 
 class GameViewController: UIViewController {
 
+    @IBOutlet weak var LivesLabel: UILabel!
+    @IBOutlet weak var ScoreLabel: UILabel!
+    
     override func viewDidLoad()
     {
         super.viewDidLoad()
@@ -21,6 +24,13 @@ class GameViewController: UIViewController {
                 }
             }
         }
+        
+        // Initialize the Lives and Score
+        CollisionManager.gameViewController = self
+        ScoreManager.Score = 0
+        ScoreManager.Lives = 5
+        updateLivesLabel()
+        updateScoreLabel()
     }
 
     override var supportedInterfaceOrientations: UIInterfaceOrientationMask
@@ -30,5 +40,15 @@ class GameViewController: UIViewController {
 
     override var prefersStatusBarHidden: Bool {
         return true
+    }
+    
+    func updateLivesLabel()
+    {
+        LivesLabel.text = "Lives: \(ScoreManager.Lives)"
+    }
+
+    func updateScoreLabel()
+    {
+        ScoreLabel.text = "Score: \(ScoreManager.Score)"
     }
 }
