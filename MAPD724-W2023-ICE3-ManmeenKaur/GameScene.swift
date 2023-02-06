0c48fbcbdb433cb4ca7d9ad028a958c8fbcafe38
manmeenkaur <manmeen.kaur.17@gmail.com>
2023-02-05 22:14:39 -0500

    added preloaded sound impluse

--- MAPD724-W2023-ICE3-ManmeenKaur/GameScene.swift
+++ MAPD724-W2023-ICE3-ManmeenKaur/GameScene.swift
@@ -1,5 +1,7 @@
 import SpriteKit
 import GameplayKit
+import AVFoundation
+import UIKit
 
 let screenSize = UIScreen.main.bounds
 var screenWidth: CGFloat?
@@ -47,6 +49,23 @@ class GameScene: SKScene
             let cloud = Cloud()
             clouds.append(cloud)
             addChild(cloud)
+            
+            // preload / prewarm impulse sounds
+            do
+            {
+                let sounds: [String] = ["thunder", "yay"]
+                for sound in sounds
+                {
+                    let path: String = Bundle.main.path(forResource: sound, ofType: "mp3")!
+                    let url:URL = URL(fileURLWithPath: path)
+                    let avPlayer: AVAudioPlayer = try AVAudioPlayer(contentsOf: url)
+                    avPlayer.prepareToPlay()
+                }
+            }
+            catch
+            {
+
+            }
         }
     }
     
